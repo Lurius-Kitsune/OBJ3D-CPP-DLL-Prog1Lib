8c619579f3f99d13efb63e74463b951603dde355
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2024-11-15 14:35:56 +0100

    Optimisation de DynamicArray (#2)
    * pourquoi ai-je une erreur
    
    * - Tab added
    
    * - Optimisation de fileStream
    
    * -Renamed DynamicArray
    
    * - Patch bug
    
    modified `DynamicArray.h`:
    - Added specialization for `DynamicArray<T*>` with pointer-specific operations.
    - Fixed `Add` method for correct element placement.
    
    * - Secu in outOfRange
    
    * - Added commentary

--- Prog1-SandBox/Main.cpp
+++ Prog1-SandBox/Main.cpp
@@ -1,13 +1,61 @@
 #include <iostream>
+#include "FileStream.h"
+#include "DisplaySystem.h"
+#include "DynamicArray.h"
 
 using namespace std;
 
-#include "SaveManager.h"
-using namespace Save;
+void TestDynamicArray();
+
+class Bob
+{
+    string name;
+public:
+
+	Bob()
+	{
+		name = "";
+	}
+
+	Bob(const string& _name)
+	{
+		name = _name;
+	}
+	string GetName()const
+	{
+		return name;
+	}
 
 int main()
 {
-	SaveManager _sm = SaveManager("uwu.txt");
-	cout << _sm.GetData<string>("ddfefdefd");
-	return 0;
+    Config();
+    FileStream _file = FileStream("bob.txt", true);
+    //_file.Write("\nBob1", _file.ComputeLenghOfFile());
+    //Console::DisplayRainbowCenterMultiLine(_file.ReadAll(), _file.ComputeLineOfFile());
+    //DISPLAY(_file.ReadAll(), true);
+
+	TestDynamicArray();
+}
+
+void TestDynamicArray()
+{
+	Bob* _bob = new Bob("Bob");
+	DynamicArray<Bob*> _tab = DynamicArray<Bob*>();
+	_tab.Add(_bob);
+	_tab.Add(new Bob(*_bob));
+	DISPLAY(_tab[0]->GetName(), true);
+	DISPLAY(_tab[1]->GetName(), true);
+	_tab.Remove(_bob, true);
+	DISPLAY(_tab[0]->GetName(), true);
+
+	try
+	{
+		_tab[1]->GetName();
+		DISPLAY(_tab[1]->GetName(), true); // Should Crash
+	}
+	catch (const exception& _excep)
+	{
+		DISPLAY(_excep.what(), true);
+	}
+	
 }
