100d11ece34e5c26d0b4a02b485f8a20f1e82520
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2024-11-15 14:54:57 +0100

    - There was memoryLeak

--- Prog1-SandBox/Main.cpp
+++ Prog1-SandBox/Main.cpp
@@ -37,5 +37,5 @@ int main()
     FileStream _file = FileStream("Thomas.txt", true);
     _file.Write("\nBob1", 0);
     //Console::DisplayRainbowCenterMultiLine(_file.ReadAll(), _file.ComputeLineOfFile());
-    DISPLAY(_file.ReadAll(), true);
+    //DISPLAY(, true);
 }
